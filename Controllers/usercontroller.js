32c83d1596dd846ad5d2982f6a3b5ce078633ed6
Habibo-group <info29@Habibo.local>
2026-02-06 10:47:24 +0300

    FD

--- Controllers/usercontroller.js
+++ Controllers/usercontroller.js
@@ -9,3 +9,22 @@ export const createUser = async (req, res) => {
   }
 };
 
+export const findAll = async (req,res) =>{
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+};
+
+export const find = async (req,res) =>{
+    try {
+        const user = await User.findById(req.params.id);
+        if(!user) 
+        return res.status(404).json({message: error.message});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+};
+
--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { createUser } from "../Controllers/usercontroller.js";
+import { createUser ,find,findAll} from "../Controllers/usercontroller.js";
 
 export const router= express.Router();
 
 router.post("/insert",createUser);
+
+router.get("/findall",findAll)
+
+router.get("/:id", find)
 
 export default router;
